217fd229da36ffcd70a7ae1bbca985edf864bdb9
Erica <ericacoelho217@gmail.com>
2018-02-16 19:50:51 -0200

    Altera header e inclui carrossel

--- header.php
+++ header.php
@@ -8,25 +8,106 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
+
+
     <title>|| Mars Hunters </title>
   </head>
   
   <body>
     <header style="border: solid 2px; background-color: #f44; border-color:#f40;">
+  
   <div class = "container"> 
     <div class = "row">
+      
+      <div class = "col-md-3">
+        <img src="mars.png" class="rounded" alt="...">
+      </div>
+      
+       
+      <div class ="col-md-6">
+       <form>
+      
+      
+       <input class="form-control" style="margin-top:5%; float: left; width: 80%;" type="search" placeholder="Buscar" aria-label="Search">
+      
+        
+
+       <button type="button" class="btn btn-outline-dark btn-sm" style="margin-top: 6%; margin-left: 1%;" >Pesquisar</button>
+        
     
-    <img src="mars.png" class="rounded float-left" alt="...">
+ 
+      </form>
+   
+   </div>
+         
+         <div class = "col-md-3">
+          <img src="icon.png" class="rounded float-right" alt="..." style="margin-top: 5%;">
     
-<nav class="navbar navbar justify-content-between">
-  <form class="form-inline">
-    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" >
-    <button class="btn btn-outline-dark my-2 my-sm-0" type="submit">Search</button>
-  </form>
-</nav>
-    
-    <hr/>
+    <p style="margin-top: 5%;"> Olá, faça seu <a href="login.php" style="color:#fff; ">Login</a> ou <a href="cadastro.php" style="color:#fff;">Cadastre-se</a></p>  
     </div>
+    </div>
+
+
+    <div class="row">
+
+ <nav class="navbar navbar-expand-lg navbar-dark bg col-md-12">
+     
+       <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+          
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+              </li>
+          
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Produtos</a>
+              </li>
+          
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Outros </a>
+              </li>
+          
+            </ul>
+          </div>
+      
+      
+   </nav>
+
+    </div>
+  </div>
 </div>
+
+
 </header>
+<p>&nbsp;</p>
+<div class = "container">
+  
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+   
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="game.jpg" alt="First slide">
+    </div>
+   
+    <div class="carousel-item">
+      <img class="d-block w-100" src="cel.jpg" alt="Second slide">
+    </div>
+   
+    <div class="carousel-item">
+      <img class="d-block w-100" src="cel2.jpg" alt="Third slide">
+    </div>
+  </div>
+ 
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+ 
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+
+</div>
+</div>
  
