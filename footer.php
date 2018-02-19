93b1cef1b953ad7d7f85f5f02684f05df441ecd6
caroline <carolinebaptista41@gmail.com>
2018-02-19 20:28:57 -0300

    adiciona footer

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
-   <?php
-include "conexao.php";
+<!DOCTYPE>
+<html>
+<body>
+ 
+ <footer >
+ 	
+ 	<h5 class="text-center" style="border: solid 2px; margin: 5px;background-color: #f44; color: #fff; height: 50px; position: static; bottom: 0; width: 100%; ">Conheça a nossa loja Mars Hunters</h5>
+ </footer>
 
-   ?>
-   <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </body>
 </html>
