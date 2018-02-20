e6253f323bbf41645cf0e411b8d9e43f66c5b935
caroline <carolinebaptista41@gmail.com>
2018-02-19 21:25:46 -0300

    atualiza footer

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
  
  <footer >
  	
- 	<h5 class="text-center" style="border: solid 2px; margin: 5px;background-color: #f44; color: #fff; height: 50px; position: static; bottom: 0; width: 100%; ">Conheça a nossa loja Mars Hunters</h5>
+ 	<h5 class="text-center" style="border: solid 2px; margin: 5px;background-color: #ffd; color: #dc3545; height: 50px; position: static; bottom: 0; width: 100%;border-color:#dc3545; padding: 2px; padding-top: 10px; ">Conheça a nossa loja Mars Hunters</h5>
  </footer>
 
   </body>
