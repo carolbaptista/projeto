ab15d67fc10f19ae74dae1061db7feea23606a7a
erica <ericacoelho217@gmail.com>
2018-02-19 21:23:17 -0300

    Atualiza cor do header

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
   <?php 
 include "conexao.php";
   ?>
-    <header style="border: solid 2px; background-color: #f44; border-color:#f40;">
+    <header style="border: solid 2px; background-color: #ffd; border-color:#f40;">
   
   <div class = "container"> 
     <div class = "row">
@@ -35,7 +35,7 @@ include "conexao.php";
       
         
 
-       <button type="button" class="btn btn-outline-dark btn-sm" style="margin-top: 6%; margin-left: 1%;" >Pesquisar</button>
+       <button type="button" class="btn btn-outline-danger btn-sm" style="margin-top: 6%; margin-left: 1%;" >Pesquisar</button>
         
     
  
@@ -44,25 +44,25 @@ include "conexao.php";
    </div>
          
          <div class = "col-md-3">
-          <img src="icon.png" class="rounded float-right" alt="..." style="margin-top: 5%;">
+          <img src="i.png" class="rounded float-right" alt="..." style="margin-top: 5%;">
     
-    <p style="margin-top: 5%;"> Olá, faça seu <a href="login.php" style="color:#fff; ">Login</a> ou <a href="cadastro.php" style="color:#fff;">Cadastre-se</a></p>  
+    <p style="margin-top: 5%;"> Olá, faça seu <a href="login.php" style="color:#000; ">Login</a> ou <a href="cadastro.php" style="color:#000;">Cadastre-se</a></p>  
     </div>
     </div>
 
-<div class="row">
-<div class = "col-md-3"></div>
+<div class="row" style="background-color:#dc3545;">
+<div class = "col-md-3" style="background-color:#dc3545;"></div>
 
 <nav class="navbar navbar-expand-lg navbar-danger bg-danger">
 
   <a class="navbar-brand" href="#" style="color:#fff;">Produtos</a>
   
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"  style="background-color:#000;"></span>
+    <span class="navbar-toggler-icon"  style="background-color:#ffd;"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
+    <ul class="navbar-nav">
       
       <li class="nav-item active">
         <a class="nav-link" href="#" style="color:#fff;">Livros</a>
