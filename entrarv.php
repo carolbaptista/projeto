18beb4940a9413f7b4bd83f70034522cd6ddcce0
erica <ericacoelho217@gmail.com>
2018-03-07 21:25:14 -0300

    Cria entrarv e loginv

---
+++ entrarv.php
@@ -0,0 +1,68 @@
+
+<body>
+
+<?php
+
+Include "header.php";
+Include "conexao.php";
+
+
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+
+
+$sql= "SELECT * FROM cadastrov WHERE email='$email' AND senha='$senha'";
+$result= $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()){
+    
+
+  $_SESSION['logado'] = 1;
+	$_SESSION['email'] = $email;
+	
+
+	echo '
+
+
+  <p>&nbsp;</p>
+   <div class="container text-center" class = "col-md-4 card border-danger mb-4" style="max-width: ; margin-left: ">
+  
+   <a href="pedidosf.php" class="btn btn-outline-danger btn-lg active" role="button" aria-pressed="true">Visualizar pedidos</a>
+   
+   <a href="alteraa.php" class="btn btn-outline-danger btn-lg active" role="button" aria-pressed="true">Alterar alguma informação</a>
+   
+  <p>&nbsp;</p>
+
+        
+          <div class = "card-header bg-danger text-white">
+            <h4 class="text-center">Nome: '.$row['nome'].'</h4>
+          </div>
+          
+          <div class = "card-body">
+            <h5 class="text-center" class="card-title">CPF: '.$row['cpf'].'</h5>
+            <h5 class="text-center" class="card-title">Endereco: '.$row['endereco'].'</h5>
+            <h5 class="text-center" class="card-title">Endereco: '.$row['cargo'].'</h5>
+          </div>
+          
+          <div class = "card-footer">
+            <h5 class="text-center" class="card-title">Bem vinda(o) '.$row['nome'].'</h5>
+          </div>
+      </div> 
+<p>&nbsp;</p>
+
+';
+	
+		
+	}
+ }
+
+else{
+	header('location: loginv.php');
+	  
+}
+Include "footer.php";
+
+?>
+</body>
---
+++ loginv.php
@@ -0,0 +1,34 @@
+
+<body>
+<?php
+include "header.php";
+include "conexao.php";
+
+?>
+
+<div class="container">
+	<h1 class="display-4" style="text-align:center; padding-top: 50px;">Faça o login</h1>
+
+	<form method="POST" action="entrarv.php" style="padding-top: 40px">
+			<div class="form-row">
+		 <div class="form-group col-md-12">
+	    <label for="inputEmail2">Email</label>
+	    <input type="email" class="form-control" id="inputemail2" placeholder="Insira o seu email" name="email" required>
+	     </div> 
+
+	   <div class="form-group col-md-12">
+	    <label for="inputPassword2">Senha</label>
+	    <input type="password" class="form-control" id="inputsenha2" placeholder="Insira o seu senha" name="senha" required>
+	  </div>
+		 	</div>
+		 
+	   <hr/>
+		 <center><button type="submit" class="btn btn-danger"> Entrar</button>
+		 </center>
+ 
+ </form>
+ </div>
+ <?php
+include "footer.php";
+ ?>
+</body>
